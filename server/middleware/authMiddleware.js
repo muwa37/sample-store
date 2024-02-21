5793873450750dfdd2991d5fd34ca50dfc771c44
nuwa37 <yond41m3@icloud.com>
2024-02-21 17:07:54 +0600

    Added registration and login

---
+++ server/middleware/authMiddleware.js
@@ -0,0 +1,14 @@
+module.exports = function (req, res, next) {
+  if (req.method === 'OPTIONS') {
+    next();
+  }
+
+  try {
+    const token = req.headers.authorization.split(' ')[1];
+    if (!token) {
+      return res.status(401).json({ message: 'user not authorized' });
+    }
+  } catch (e) {
+    res.status(401).json({ message: 'user not authorized' });
+  }
+};
