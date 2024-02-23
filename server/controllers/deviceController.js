57ce353534f66c7b6aec1d5964f8828a00891403
nuwa37 <yond41m3@icloud.com>
2024-02-23 19:41:36 +0600

    Added backend basket logic

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -17,7 +17,6 @@ class DeviceController {
         typeId,
         img: fileName,
       });
-
       if (info) {
         info = JSON.parse(info);
         info.forEach(i =>
@@ -28,7 +27,6 @@ class DeviceController {
           })
         );
       }
-
       return res.json(device);
     } catch (e) {
       next(ApiError.badRequest(e.message));
@@ -36,36 +34,42 @@ class DeviceController {
   }
 
   async getAll(req, res) {
-    let { brandId, typeId, limit, page } = req.query;
-    page = page || 1;
-    limit = limit || 9;
-    let offset = page * limit - limit;
-    let devices;
-    if (!brandId && !typeId) {
-      devices = await Device.findAndCountAll({ limit, offset });
+    try {
+      let { brandId, typeId, limit, page } = req.query;
+      page = page || 1;
+
+      limit = limit || 2;
+
+      let offset = page * limit - limit;
+      let devices;
+      if (!brandId && !typeId) {
+        devices = await Device.findAndCountAll({ limit, offset });
+      }
+      if (brandId && !typeId) {
+        devices = await Device.findAndCountAll({
+          where: { brandId },
+          limit,
+          offset,
+        });
+      }
+      if (!brandId && typeId) {
+        devices = await Device.findAndCountAll({
+          where: { typeId },
+          limit,
+          offset,
+        });
+      }
+      if (brandId && typeId) {
+        devices = await Device.findAndCountAll({
+          where: { typeId, brandId },
+          limit,
+          offset,
+        });
+      }
+      return res.json(devices);
+    } catch (e) {
+      console.log(e);
     }
-    if (brandId && !typeId) {
-      devices = await Device.findAndCountAll({
-        where: { brandId },
-        limit,
-        offset,
-      });
-    }
-    if (!brandId && typeId) {
-      devices = await Device.findAndCountAll({
-        where: { typeId },
-        limit,
-        offset,
-      });
-    }
-    if (brandId && typeId) {
-      devices = await Device.findAndCountAll({
-        where: { typeId, brandId },
-        limit,
-        offset,
-      });
-    }
-    return res.json(devices);
   }
 
   async getOne(req, res) {
